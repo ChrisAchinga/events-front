b2d027b21bd9ec2b1c724049b053a5c343851e2e
ChrisAchinga <achinga.chris@gmail.com>
2020-08-21 01:50:29 +0300

    update layout

--- article/admin.py
+++ article/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Article, Category
+from .models import Post, Category
 
-admin.site.register(Article)
+admin.site.register(Post)
 admin.site.register(Category)
