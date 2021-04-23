a4b6e3cbbd240eb2642b314b4898c657b0a65a9e
ChrisAchinga <achinga.chris@gmail.com>
2021-04-23 14:23:12 +0300

    eventpages

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,16 @@
 import Layout from '@/components/Layout'
+import EventItem from '@/components/EventItem'
 import { API_URL } from '@/config/index'
 
-export default function Home() {
+export default function Home({ events }) {
   return (
     <Layout>
       <h1>Upcoming Events</h1>
+      {events.length === 0 && <h3>No Events Available</h3>}
+
+      {events.map((evt) => (
+        <EventItem key={evt.id} evt={evt} />
+      ))}
     </Layout>
   )
 }
@@ -12,10 +18,10 @@ export default function Home() {
 export async function getServerSideProps() {
   const res = await fetch(`${API_URL}/api/events`)
   const events = await res.json()
-
-  console.log(events)
+  // console.log(events)
 
   return {
     props: { events },
+    // revalidate: 1,
   }
 }
